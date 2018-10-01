7cca9f6b6949d546b359bbaa327cd610095d6196
sguillen18 <36303985+sguillen18@users.noreply.github.com>
2018-10-01 14:59:26 -0400

    Add files via upload

---
+++ InfixToPostfix.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+
+public class InfixToPostfix {
+
+	public String convert (String infix) {
+		
+		//new empty stack
+		Stack operatorStack = new Stack();
+		
+		//while parsing through
+		while(infex[i] < ) {
+			
+			char nextCharacter;
+			switch (nextCharacter) {
+			case '^':
+				operatorStack.push(nextCharacter);
+				break;
+			case '+':
+			case '-':
+			case '/':
+				while(!operatorStack.isEmpty() || precedence)
+			}
+			
+		}
+		return infix;
+	}
+}
