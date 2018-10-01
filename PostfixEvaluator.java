f9e92a6b4593862eb85c77608180700d7cfd81fd
sguillen18 <36303985+sguillen18@users.noreply.github.com>
2018-10-01 17:45:12 -0400

    Add files via upload

--- PostfixEvaluator.java
+++ PostfixEvaluator.java
@@ -1,4 +1,43 @@
+import java.util.Stack;
 
 public class PostfixEvaluator {
 
+	public String eval (String postfix) {
+		//new empty stack
+		Stack <Character> operatorStack = new Stack <Character>();
+		
+		int index = 0;
+	    int characterCount = postfix.length();
+	    char nextCharacter;
+	    char operandOne;
+	    char operandTwo;
+	    char result;
+		
+		//new empty stack
+		Stack <Character> valueStack = new Stack <Character>();
+		
+		//while parsing through
+		while(index < characterCount) {
+			switch (nextCharacter) {
+			case '+':
+			case '-':
+			case '*':
+			case '/':
+			case '^':
+				operandTwo = valueStack.pop();
+				operandOne = valueStack.pop();
+				result = 
+				valueStack.push(result);
+				break;
+			default:
+				break;
+			}
+			
+		}
+
+			
+		return postfix;
+	}
+	
 }
+
